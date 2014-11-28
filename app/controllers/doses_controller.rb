3b35f880a82a421b9032fd2b9987af0015c283ba
flros <florian.rossini@gmail.com>
2014-11-28 15:11:54 +0100

    correction thomas - choix ingredient fixed

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,13 +1,30 @@
 class DosesController < ApplicationController
+  before_action :get_cocktail
 
   def new
+    @ingredients = Ingredient.all
+    @dose = Dose.new(cocktail: @cocktail)
   end
 
   def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
-  def destroy
+private
+
+  def dose_params
+    params.require(:dose).permit(:amount, :ingredient_id)
   end
 
+  def get_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
 
 end
